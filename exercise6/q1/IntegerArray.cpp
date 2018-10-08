0c37871ee6af34cd53e2d19a00ed3689a57d138e
Edward Lim <eyl215@tui.doc.ic.ac.uk>
2018-10-08 23:47:24 +0100

    Started exercise 6

---
+++ exercise6/q1/IntegerArray.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include"IntegerArray.h"
+#include <cmath>
+
+using namespace std;
+
+void input_array(int list[], int n){
+  for(int i=0;i<n;++i){
+    cout << "Please input the integer: ";
+    cin >> list[i];
+  }
+}
+
+void display_array(int list[], int n){
+  cout.width(15);
+  cout << "index" << "element" << endl;
+  for(int i=0;i<n;++i){
+    cout.width(15);
+    cout << i << list[i] << endl;
+  }
+}
+
+void copy_array(int list1[], int list2[], int n){
+  for(int i=0;i<n;++i){
+    list1[i]=list2[i];
+  }
+}
+
+float average(int list[], int n){
+  int sum;
+  for(int i=0;i<n;++i){
+    sum+=list[i]
+  }
+  return static_cast<float>(sum)/n;
+}
+
+float standard_deviation(int list[], int n){
+  float sum;
+  for(int i=0;i<n;i++){
+    sum += pow(list[i]-average(list[], n),2);
+  }
+  return sqrt(sum/n);
+}
---
+++ exercise6/q1/IntegerArray.h
@@ -0,0 +1,12 @@
+#ifndef INTEGER_ARRAY_H
+#define INTEGER_ARRAY_H
+
+const int MAX_ARRAY_SIZE = 1000;
+
+void input_array(int list[],int n);
+void display_array(int list[], int n);
+void copy_array(int list1[], int list2[], int n);
+float average(int list[], int n);
+float standard_deviation(int list[], int n);
+
+#endif
