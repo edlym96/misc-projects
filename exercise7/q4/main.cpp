ced7588718c65130f786d13d627ae2f80ca4a066
edlym96 <edlym96@hotmail.com>
2018-10-12 19:51:27 +0100

    Added in delete node function

--- exercise7/q4/main.cpp
+++ exercise7/q4/main.cpp
@@ -22,8 +22,10 @@ void print_list(Node_ptr a_node);
 
 void add_after(Node_ptr &list, char a_word[], char word_after[]);
 
+void delete_node(Node_ptr &a_list, char a_word[]);
+
 int main(){
-  char a_word[MAX_WORD_LENGTH],new_word[MAX_WORD_LENGTH];
+  char a_word[MAX_WORD_LENGTH],new_word[MAX_WORD_LENGTH],del_word[MAX_WORD_LENGTH];
   Node_ptr my_list = NULL;
 
   assign_list(my_list);
@@ -36,6 +38,13 @@ int main(){
   cin >> a_word;
 
   add_after(my_list, a_word, new_word);
+
+  print_list(my_list);
+
+  cout << "DELETE WORD: " << endl;
+  cin >> del_word;
+  
+  delete_node(my_list,del_word);
 
   print_list(my_list);
   
@@ -99,8 +108,25 @@ void add_after(Node_ptr &a_list,char a_word[], char word_after[]){
     }
     p=p->ptr_to_next_node;
   }
-  
   return;
-  
+}
+
+void delete_node(Node_ptr &a_list, char a_word[]){
+  Node_ptr p=a_list, q; //PROBLEM IS THERE IS 2 SPACES AFTER DELETING MEANING THE OBJET IS STILL IN LIST EVENTHO ITS DELETED
+  while(p){ //check p
+    if(!strcmp(a_word,p->word)){ //checl for similarity
+      if(q){ //if q is not empty, make q (node before node to be deleted to point to the next node of the node to be deleted)
+	q->ptr_to_next_node=p->ptr_to_next_node;
+      }else{
+	a_list=p->ptr_to_next_node;
+      }
+      delete p; //delete p
+      p=NULL; // make p null
+      return;
+    }
+    q=p; //if not the same, assign q to p;
+    p=p->ptr_to_next_node; //point to next node
+  }
+  return;
 }
 
