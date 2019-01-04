d9ad602d38c99862758275882cc7347f0a70c40d
edlym96 <edlym96@hotmail.com>
2019-01-04 19:50:12 +0000

    finished river

--- river/main.cpp
+++ river/main.cpp
@@ -35,20 +35,20 @@ int main() {
   cout << "The initial state: "<< endl;
   scene = make_river_scene("BCCCMMM","");
   print_scene(scene);  
-
+  
   cout << "====================== Question 2 ======================" << endl << endl;
 
   char left[10] = "MMMB";
   int result = perform_crossing(left, "MM");
   cout << status_description(result) << endl << endl;
   cout << "After crossing left = " << left << endl;
-
+  
   cout << "====================== Question 3 ======================" << endl << endl;
 
   result = play_game();
 
   cout << "The game ended as follows: " << status_description(result) << endl;
-
+  /*
   cout << "=================== Bonus Challenge ====================" << endl << endl;
 
   char answer[512];
@@ -57,6 +57,6 @@ int main() {
     cout << "Solution is " << answer << endl;
   else
     cout << "Solution does not exist." << endl;
-
+  */
   return 0;
 }
--- river/river.cpp
+++ river/river.cpp
@@ -117,3 +117,145 @@ const char *status_description(int code) {
 
 /* insert your functions here */
 
+char** make_river_scene(const char* left, const char* boat){
+  char** scene = create_scene();
+  add_to_scene(scene, 0, 0, "bank.txt");
+  add_to_scene(scene, 0, 53, "bank.txt");
+  add_to_scene(scene, 3, 30, "sun.txt");
+  add_to_scene(scene, 19, 19, "river.txt");
+  int missionary_count = 0;
+  int cannibal_count = 0;
+  bool boat_on_left = false;
+  while(*left!='\0'){
+    if(*left == 'M'){
+      add_to_scene(scene, 2, 1+missionary_count*6, "missionary.txt");
+      missionary_count++;
+    }else if(*left == 'C'){
+      add_to_scene(scene, 11, 1+cannibal_count*6, "cannibal.txt");
+      cannibal_count++;
+    }else if(*left == 'B'){
+      add_to_scene(scene, 17, 19, "boat.txt");
+      boat_on_left = true;
+    }
+    ++left;
+  }
+  if(!boat_on_left){
+    add_to_scene(scene, 17, 36, "boat.txt");
+  }
+  int spot = 0;
+  while(*boat != '\0'){
+    if(boat_on_left){
+      if(*boat == 'M'){
+	add_to_scene(scene, 11, 20+spot*6, "missionary.txt");
+	missionary_count++;
+	++spot;
+      }else if(*left == 'C'){
+	add_to_scene(scene, 11, 20+spot*6, "cannibal.txt");
+	cannibal_count++;
+	++spot;
+      }
+    }else{
+      if(*boat == 'M'){
+	add_to_scene(scene, 11, 39+spot*6, "missionary.txt");
+	missionary_count++;
+	++spot;
+      }else if(*left == 'C'){
+	add_to_scene(scene, 11, 39+spot*6, "cannibal.txt");
+	cannibal_count++;
+	++spot;
+      }
+    }
+    ++boat;
+  }
+  for(int i=0; i<3-missionary_count; ++i){
+    add_to_scene(scene, 2, 54+i*6, "missionary.txt");
+  }
+  for(int i=0; i<3-cannibal_count; ++i){
+    add_to_scene(scene, 11, 54+i*6, "cannibal.txt");
+  }
+  return scene;
+}
+
+int perform_crossing(char* left, const char* target){
+  int boat_on_left = false;
+  int missionary_bank = 0;
+  int cannibal_bank = 0;
+  char* left_cpy = left;
+  if(strlen(target)>2 || strlen(target)<1){
+    return ERROR_INVALID_MOVE;
+  }
+  while(*left_cpy!='\0'){
+    if(*left_cpy == 'M'){
+      missionary_bank++;
+    }else if(*left_cpy == 'C'){
+      cannibal_bank++;
+    }else if(*left_cpy == 'B'){
+      boat_on_left = true;
+    }else{
+      return ERROR_INVALID_MOVE;
+    }
+    ++left_cpy;
+  }
+  if(!boat_on_left){
+    missionary_bank = 3-missionary_bank;
+    cannibal_bank = 3-cannibal_bank;
+  }
+  int missionary_count = 0;
+  int cannibal_count = 0;
+  while(*target!='\0'){
+    if(*target == 'M'){
+      missionary_count++;
+    }else if(*target == 'C'){
+      cannibal_count++;
+    }else{
+      return ERROR_INVALID_MOVE;
+    }
+    target++;
+  }
+
+  missionary_bank-=missionary_count;
+  cannibal_bank-=cannibal_count;
+  if(missionary_bank <0 || cannibal_bank < 0){
+    return ERROR_INVALID_MOVE;
+  }
+  if(missionary_bank>0 && cannibal_bank>missionary_bank){
+    return ERROR_MISSIONARIES_EATEN;
+  }
+
+  if(!boat_on_left){
+    missionary_bank = 3-missionary_bank;
+    cannibal_bank = 3-cannibal_bank;
+  }
+  
+  int i=0;
+  for(; i<cannibal_bank; ++i){
+    left[i] = 'C';
+  }
+  for(; i<cannibal_bank+missionary_bank; ++i){
+    left[i] = 'M';
+  }
+  if(!boat_on_left){
+    left[i] = 'B';
+    ++i;
+  }
+  left[i] = '\0';
+  if(left[0] == '\0'){
+    return VALID_GOAL_STATE;
+  }else{
+    return VALID_NONGOAL_STATE;
+  }
+}
+
+int play_game(){
+  char move[16];
+  char left[16] = "CCCMMMB";
+  int code = VALID_NONGOAL_STATE;
+  while(code != VALID_GOAL_STATE && code != ERROR_MISSIONARIES_EATEN){
+    char** scene = make_river_scene(left, "");
+    print_scene(scene);
+    cin >> move;
+    code = perform_crossing(left, move);
+    cout << status_description(code)<< endl;
+  }
+  return code;
+}
